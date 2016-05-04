5744eb2fd847b0cc08a45fe33b2d80c6a149e90c
LetiBadillo <letibadillog@hotmail.com>
2016-05-04 06:49:21 +0300

    Update Bitacora.java

--- POO-EquipoAlfa/src/poo/equipoalfa/Bitacora.java
+++ POO-EquipoAlfa/src/poo/equipoalfa/Bitacora.java
@@ -24,31 +24,87 @@ import java.sql.*;
  * @author LetiBadillo
  */
 
+
 public class Bitacora {
+    private static int a = 0;
     private static boolean o=false;
     public static boolean registrarCompra(String IDTarjeta, String ticket, String cantidad) throws SQLException {
         
         try{
-        mysqlConnection c = new mysqlConnection();
-        c.conexion();
-        Connection co= c.conexion();
-            String query = "insert into registrodecompra (numtarjetaR, ticket, cantidad) values (?, ?, ?)";
-      PreparedStatement preparedStmt = co.prepareStatement(query);
-      preparedStmt.setString (1, IDTarjeta);
-      preparedStmt.setString (2, ticket);
-      preparedStmt.setString (3,cantidad);
+        mysqlConnection c = new mysqlConnection(); //crea una nueva conexión a la bd
+        c.conexion();                              //establece conexión
+        Connection co= c.conexion();               //crea una conexión co
+        String chequeo = "select idtarjeta from tarjetas where idtarjeta="+IDTarjeta;       //verifica que la tarjeta exista en la bd
+        ResultSet rs;                  //Resultset que recibe el resultado del query
+        Statement stmt= co.createStatement();   //statement que ejecuta el query 
+        rs = stmt.executeQuery(chequeo);        //guarda el resultado del query
+        int count = 0;                          //contador de filas
+
+        while (rs.next()) {                     //mientras haya resultados va a contar las filas
+              ++count;
+    
+            }
+
+if (count == 1) {                   //si arroja un resultado
+    try {
+        String query = "insert into registrodecompra (numtarjetaR, ticket, cantidad) values (?, ?, ?)"; //inserta los campos puestos por el cliente en la tabla de registros de compra
+    
+      PreparedStatement preparedStmt = co.prepareStatement(query);      //statement que ejecuta el query
+      preparedStmt.setString (1, IDTarjeta);            //guarda ID tarjeta en la columna 1
+      preparedStmt.setString (2, ticket);               //guarda el ticket en la columna 2
+      preparedStmt.setString (3,cantidad);              //guarda la cantidad en la columna 3
       int r= preparedStmt.executeUpdate();
-      if (r!= 0){
+      if (r== 1){                                       //si el query se ejecuta entonces devuelve true
           o=true;
+          a = 1;
+          
+      }else {
+          o=false;
       }
-        }
+    } 
       catch (SQLException e){
-          JOptionPane.showMessageDialog(null, "Error"+e);
+          JOptionPane.showMessageDialog(null, "El ticket ya fue registrado.");
       }
       
         catch(Exception r){
             JOptionPane.showMessageDialog(null, "Error"+r);
         }
+        if(a == 1){            //si sí se hizo el insert, añade el porcentaje al saldo de la tarjeta
+          double incremento = MonederoElectronico.getIncremento()/100*Double.parseDouble(cantidad);
+          try{
+        mysqlConnection con = new mysqlConnection();
+        con.conexion();
+        Connection cc= con.conexion();
+     
+      String query = "UPDATE tarjetas SET saldo= saldo+" +incremento+" WHERE idTarjeta="+IDTarjeta;
+      PreparedStatement stTarjetas = cc.prepareStatement(query);
+      int b= stTarjetas.executeUpdate();
+       if (b!=0){
+           o= true;
+       }}
+      catch (SQLException e){
+          JOptionPane.showMessageDialog(null, "Error"+e);
+      }
+      
+        catch(Exception q){
+            JOptionPane.showMessageDialog(null, "Error"+q);
+        }
+      }
+}
+if(count == 0){
+    JOptionPane.showMessageDialog(null, "El número de tarjeta es inválido");
+    o = false;
+}
+        }catch (SQLException e){
+          JOptionPane.showMessageDialog(null, "Error"+e);
+      }
+      
+        catch(Exception q){
+            JOptionPane.showMessageDialog(null, "Error"+q);
+        }
+       
+
+            
             return o;
         }
     }
